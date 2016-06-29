dcd07b240fbc2f18c5dd75eb5d51ae558aa413bc
aemal <sayer.aemal@gmail.com>
2016-06-29 12:00:56 +0200

    save

--- websocket/app.js
+++ websocket/app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
 	app = express(),
-	server = require('http').createServer(app),
+	server = require('https').createServer(app),
 	io = require('socket.io').listen(server);
 
 server.listen(9090);
