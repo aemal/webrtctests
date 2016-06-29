9e13c5a40b6412968d3e85434b4fe22c4e099774
aemal <sayer.aemal@gmail.com>
2016-06-29 18:31:50 +0200

    save

--- websocket/app.js
+++ websocket/app.js
@@ -1,9 +1,25 @@
-var express = require('express'),
+var https = require('https'),     
+    fs =    require('fs');        
+
+var options = {
+    key:    fs.readFileSync('/etc/cert/privkey.pem'),
+    cert:   fs.readFileSync('/etc/cert/cert.pem')//,
+    //ca:     fs.readFileSync('ssl/ca.crt') //we don't know yet what is it
+};
+
+var app = https.createServer(options);
+io = require('socket.io').listen(app);     //socket.io server listens to https connections
+//app.listen(8895, "0.0.0.0");
+app.listen(9090);
+
+//==========================================================================
+
+/*var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 
-server.listen(9090);
+server.listen(9090);*/
 
 app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/index.html');
