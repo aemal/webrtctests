31466e9e83fe1b15479dcbaf6bb0fc437934bae5
aemal <sayer.aemal@gmail.com>
2016-06-29 11:54:38 +0200

    save

---
+++ websocket/app.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+server.listen(9090);
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', data);////this sends it to everyone, including the sender.
+		//socket.broadcast.emit('new message', data);//sends it to everyone except the sender.
+	});
+});
